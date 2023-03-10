a681902b771eb295b2c925733cfd88a7c68f3bcc
Myrmex <dfusi@hotmail.com>
2023-03-10 19:14:59 +0100

    Initial commit

---
+++ CadmusChgcApi/Services/ChgcRepositoryProvider.cs
@@ -0,0 +1,73 @@
+﻿using System.Reflection;
+using Cadmus.Core;
+using Cadmus.Core.Config;
+using Cadmus.Core.Storage;
+using Cadmus.Mongo;
+using Cadmus.General.Parts;
+using Cadmus.Philology.Parts;
+
+namespace CadmusChgcApi.Services;
+
+/// <summary>
+/// Cadmus Chgc repository provider.
+/// </summary>
+/// <seealso cref="IRepositoryProvider" />
+public sealed class ChgcRepositoryProvider : IRepositoryProvider
+{
+    private readonly IPartTypeProvider _partTypeProvider;
+
+    /// <summary>
+    /// The connection string.
+    /// </summary>
+    public string ConnectionString { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChgcRepositoryProvider"/>
+    /// class.
+    /// </summary>
+    public ChgcRepositoryProvider()
+    {
+        ConnectionString = "";
+        TagAttributeToTypeMap map = new();
+        map.Add(new[]
+        {
+            // Cadmus.General.Parts
+            typeof(NotePart).GetTypeInfo().Assembly,
+            // Cadmus.Philology.Parts
+            typeof(ApparatusLayerFragment).GetTypeInfo().Assembly,
+            // Cadmus.Chgc.Parts currently does not exist
+            // typeof(MYPART).GetTypeInfo().Assembly,
+        });
+
+        _partTypeProvider = new StandardPartTypeProvider(map);
+    }
+
+    /// <summary>
+    /// Gets the part type provider.
+    /// </summary>
+    /// <returns>part type provider</returns>
+    public IPartTypeProvider GetPartTypeProvider()
+    {
+        return _partTypeProvider;
+    }
+
+    /// <summary>
+    /// Creates a Cadmus repository.
+    /// </summary>
+    /// <returns>repository</returns>
+    public ICadmusRepository CreateRepository()
+    {
+        // create the repository (no need to use container here)
+        MongoCadmusRepository repository = new(_partTypeProvider,
+                new StandardItemSortKeyBuilder());
+
+        repository.Configure(new MongoCadmusRepositoryOptions
+        {
+            ConnectionString = ConnectionString ??
+            throw new InvalidOperationException(
+                "No connection string set for IRepositoryProvider implementation")
+        });
+
+        return repository;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Cadmus CHGC Proof of Concept
+
+Quick Docker image build:
+
+    docker build . -t vedph2020/cadmus-chgc-api:0.0.1 -t vedph2020/cadmus-chgc-api:latest
+
+(replace with the current version).
+
+This is a Cadmus API layer customized for the CHGC project (*Cadmus Compendium Historiae in genealogia Christi*).
